44e13d7ac0c6d5c111a930b7cca54da139dc7760
Earl Cameron <cam.cameron@ukg.com>
2024-01-26 23:54:44 -0500

    cleaned up files

--- controller/MonthlyDebts.go
+++ controller/MonthlyDebts.go
@@ -9,73 +9,78 @@ import (
 	"time"
 )
 
-func AddDebt() {
-	fmt.Println("test")
-}
-
+// GetBills handles the HTTP request to retrieve bill information.
+// It can return a specific bill if an ID is provided, or all bills otherwise.
 func GetBills(w http.ResponseWriter, r *http.Request) {
+	// Extract the 'id' query parameter
 	id := r.URL.Query().Get("id")
+
+	// If 'id' parameter is provided
 	if id != "" {
-		// Convert id to integer
+		// Convert 'id' to an integer
 		intID, err := strconv.Atoi(id)
 		if err != nil {
-			// Handle error if id is not a valid integer
+			// Respond with error if 'id' is not a valid integer
 			http.Error(w, "Invalid ID format", http.StatusBadRequest)
 			return
 		}
 
-		// Get bill by ID
+		// Retrieve the bill by its ID
 		bill := structs.RecurringBills.GetByID(intID)
 		if bill == nil {
-			// Handle error if bill not found
+			// Respond with error if the bill is not found
 			http.Error(w, "Bill not found", http.StatusNotFound)
 			return
 		}
 
-		// Create a list with a single bill
+		// Prepare a list containing the found bill
 		bills := structs.RecurringBillList{Bills: []structs.RecurrinBill{*bill}}
 
-		// Serve text/html
+		// Set the Content-Type of the response to text/html
 		w.Header().Set("Content-Type", "text/html")
-		// Render the component to the response writer
+		// Render the bill information as HTML to the response writer
 		components.RecurringBillsComponent(bills).Render(r.Context(), w)
 	} else {
-
+		// Extract the 'sort' query parameter
 		sort := r.URL.Query().Get("sort")
+		// Sort bills if 'sort' parameter is provided
 		if sort != "" {
 			structs.RecurringBills.SortBy(sort)
 		}
 
-		// Serve all bills when no ID is provided
+		// Respond with all bills when no 'id' is provided
 		component := components.RecurringBillsComponent(structs.RecurringBills)
 		w.Header().Set("Content-Type", "text/html")
 		component.Render(r.Context(), w)
 	}
 }
 
+// UpdateBills handles the HTTP request for updating bill information.
+// For a GET request, it fetches a specific bill for editing or prepares a new bill.
+// For other request types, it updates a bill's details.
 func UpdateBills(w http.ResponseWriter, r *http.Request) {
-	// Check if the request method is GET and an ID is provided
+	// Handle GET request
 	if r.Method == "GET" {
-		fmt.Println("GET")
 		id := r.URL.Query().Get("id")
 		if id != "" {
-			// Convert id to integer
+			// Fetch and edit an existing bill
 			intID, err := strconv.Atoi(id)
 			if err != nil {
-				// Handle error if id is not a valid integer
 				http.Error(w, "Invalid ID format", http.StatusBadRequest)
+				return
 			}
 			bill := structs.RecurringBills.GetByID(intID)
-			if err != nil {
-				// Handle error if bill not found
+			if bill == nil {
 				http.Error(w, "Bill not found", http.StatusNotFound)
+				return
 			}
 			w.Header().Set("Content-Type", "text/html")
 			components.EditRecurringBillsComponent(*bill, true).Render(r.Context(), w)
 		} else {
-			newId := structs.RecurringBills.GetLastID()
+			// Set up a new bill
+			newID := structs.RecurringBills.GetLastID() + 1
 			bill := structs.RecurrinBill{
-				Id:         newId + 1,
+				Id:         newID,
 				Name:       "",
 				Amount:     0,
 				DayOfMonth: 0,
@@ -88,25 +93,24 @@ func UpdateBills(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 1. Parse URL Parameters
+	// Handle POST request for updating a bill
+	// Parse and validate the bill ID
 	billID := r.URL.Query().Get("id")
 	if billID == "" {
 		http.Error(w, "No bill ID provided", http.StatusBadRequest)
 		return
 	}
-
 	id, err := strconv.Atoi(billID)
 	if err != nil {
 		http.Error(w, "Invalid bill ID", http.StatusBadRequest)
 		return
 	}
 
-	// 2. Parse Form Data
+	// Parse form data
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Error parsing form", http.StatusInternalServerError)
 		return
 	}
-
 	name := r.FormValue("name")
 	amount, err := strconv.ParseFloat(r.FormValue("amount"), 64)
 	if err != nil {
@@ -123,88 +127,95 @@ func UpdateBills(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 3. Find and Update Bill
-	bill := structs.RecurringBills.GetByID(id) // Assuming this function exists
-	if err != nil {
+	// Find and update the bill
+	bill := structs.RecurringBills.GetByID(id)
+	if bill == nil {
 		http.Error(w, "Bill not found", http.StatusNotFound)
 		return
 	}
-
 	bill.Name = name
 	bill.Amount = amount
 	bill.DayOfMonth = dayOfMonth
 	bill.Notes = notes
 
-	// 4. Add to Bill List
-	bills := structs.RecurringBillList{Bills: []structs.RecurrinBill{*bill}}
-
-	// 5. Render Component
+	// Render updated bill information
 	w.Header().Set("Content-Type", "text/html")
-	components.RecurringBillsComponent(bills).Render(r.Context(), w)
+	components.RecurringBillsComponent(structs.RecurringBillList{Bills: []structs.RecurrinBill{*bill}}).Render(r.Context(), w)
 }
 
+// AddBills handles the HTTP POST request to add a new bill
+// It parses the form data and adds a new bill to the list
 func AddBills(w http.ResponseWriter, r *http.Request) {
+	// Ensure the request method is POST
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// 2. Parse Form Data
+	// Parse the form data from the request
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Error parsing form", http.StatusInternalServerError)
 		return
 	}
 
+	// Retrieve and validate the 'name' field
 	name := r.FormValue("name")
 	if name == "" {
 		http.Error(w, "No name provided", http.StatusBadRequest)
 		return
 	}
 
+	// Retrieve and validate the 'amount' field
 	amount, err := strconv.ParseFloat(r.FormValue("amount"), 64)
 	if err != nil {
 		http.Error(w, "Invalid amount", http.StatusBadRequest)
 		return
 	}
+
+	// Retrieve other form fields
 	date := r.FormValue("date")
 	notes := r.FormValue("notes")
 
-	// Convert date to day of month
+	// Convert the date to a day of the month
 	dayOfMonth, err := getDayOfMonth(date)
 	if err != nil {
 		http.Error(w, "Invalid date", http.StatusBadRequest)
 		return
 	}
 
+	// Generate a new ID for the bill
 	id := structs.RecurringBills.GetLastID() + 1
 
+	// Create a new bill instance
 	bill := structs.RecurrinBill{
 		Id:         id,
 		Name:       name,
 		Amount:     amount,
 		DayOfMonth: dayOfMonth,
-		Owner:      "cameron",
+		Owner:      "cameron", // This could be dynamic based on context
 		Notes:      notes,
 	}
 
+	// Append the new bill to the list
 	structs.RecurringBills.Bills = append(structs.RecurringBills.Bills, bill)
 
-	// send headers
+	// Set response headers and status
 	w.Header().Set("HX-Trigger", "newBill")
-	// reurn a 200 OK response
 	w.WriteHeader(http.StatusOK)
-	// write a message to the response writer
-	fmt.Fprintf(w, "")
+	// Optional: Write a confirmation message to the response
+	fmt.Fprintln(w, "Bill added successfully")
 }
 
+// DeleteBills handles the HTTP DELETE request to remove a specific bill.
+// It parses the bill ID from the URL parameters and removes the bill from the list.
 func DeleteBills(w http.ResponseWriter, r *http.Request) {
-
+	// Ensure the request method is DELETE
 	if r.Method != http.MethodDelete {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// 1. Parse URL Parameters
+	// Parse and validate the bill ID from URL parameters
 	billID := r.URL.Query().Get("id")
 	if billID == "" {
 		http.Error(w, "No bill ID provided", http.StatusBadRequest)
@@ -217,21 +228,20 @@ func DeleteBills(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 2. Find and Delete Bill
-	bill := structs.RecurringBills.GetByID(id) // Assuming this function exists
+	// Find the bill by ID and handle if not found
+	bill := structs.RecurringBills.GetByID(id)
 	if bill == nil {
 		http.Error(w, "Bill not found", http.StatusNotFound)
 		return
 	}
 
-	// 3. Remove from Bill List
+	// Remove the bill from the list
 	structs.RecurringBills.RemoveByID(id)
 
-	// 5. Render Component
-	// reurn a 200 OK response
+	// Set the response status to 200 OK
 	w.WriteHeader(http.StatusOK)
-	// write a message to the response writer
-	fmt.Fprintf(w, "")
+	// Optional: Write a confirmation message to the response
+	fmt.Fprintln(w, "Bill deleted successfully")
 }
 
 // Helper function to convert date string to day of month
