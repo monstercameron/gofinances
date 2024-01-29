1cd3d1c44ee5a22b1fe58a788f93ce2f873be089
Earl Cameron <cam.cameron@ukg.com>
2024-01-29 02:45:19 -0500

    setup for next session

---
+++ features/settings/Settings.go
@@ -0,0 +1,18 @@
+package settings
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("settings.init()")
+}
+
+
+// what settings do I want to store?
+
+// add other people to the account
+// choose a list of financial strategies
+// add open ai API key
+// export sqlite database
+// import sqlite database
